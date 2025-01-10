34525f9c8aa0fb93882599f8d8a9c566c8245b8e
Rodrigo Vittori <vittorirodrigo@gmail.com>
2025-01-09 21:59:16 -0300

    Pusheamos en main

--- main.bat
+++ main.bat
@@ -2,4 +2,4 @@
 REM # Programa de ejemplo
 REM # Curso de Unity - Clase M7.L2 - Actividad #4 "Revert y Branch"
 echo. El curso 3069 es el mejor!
-echo. No todos los cambios son para mejor :(
+echo. Volvi a main!
