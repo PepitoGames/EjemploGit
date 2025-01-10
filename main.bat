3c590ff61bfa7aeaa32e79515ddcf18a05fbfbfb
Rodrigo Vittori <vittorirodrigo@gmail.com>
2025-01-09 21:40:47 -0300

    Revert "Actividad Nº 2 - Actualizar"
    This reverts commit 1464a6dc1d846e08c329f3e578cba19602f3e48c.

--- main.bat
+++ main.bat
@@ -1,5 +1,4 @@
 @Echo off
 REM # Programa de ejemplo
-REM # Curso de Unity - Clase M7.L2 - Actividad #3 "Commit y Push"
+REM # Curso de Unity - Clase M7.L2 - Actividad #2 "Creando el Proyecto"
 echo. El curso 3069 es el mejor!
-echo. El secreto del One Piece es: [REDACTED]
