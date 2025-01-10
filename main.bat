3be7d43e71e6664afc3a239c33b9efabd6b1a6a2
Rodrigo Vittori <vittorirodrigo@gmail.com>
2025-01-09 22:09:21 -0300

    Ejemplo desarrollo en dev
    Estoy programando cambios que NO quiero en mi proyecto final

--- main.bat
+++ main.bat
@@ -3,4 +3,5 @@ REM # Programa de ejemplo
 REM # Curso de Unity - Clase M7.L2 - Actividad #4 "Revert y Branch"
 echo. El curso 3069 es el mejor!
 echo. Mirá mamá: me independicé! :)
-echo. Volvi a dev :D
+echo. Vuelvo a agregar cambios -> me voy a dev
+echo. estoy programando obstaculos
