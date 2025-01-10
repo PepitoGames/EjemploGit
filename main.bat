25c1fe89f188977793008724d4a6a7af2aff9166
Rodrigo Vittori <vittorirodrigo@gmail.com>
2025-01-09 21:42:44 -0300

    Actividad 4: Deshacer cambios
    Porque no todo cambio es para mejor

--- main.bat
+++ main.bat
@@ -1,4 +1,5 @@
 @Echo off
 REM # Programa de ejemplo
-REM # Curso de Unity - Clase M7.L2 - Actividad #2 "Creando el Proyecto"
+REM # Curso de Unity - Clase M7.L2 - Actividad #4 "Revert y Branch"
 echo. El curso 3069 es el mejor!
+echo. No todos los cambios son para mejor :(
